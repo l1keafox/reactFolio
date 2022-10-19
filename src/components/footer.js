94a9773762b2bb3f51a8b872574fc94277c16ed5
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-18 20:06:16 -0600

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -38,12 +38,14 @@ export default function footer() {
   const icon = "w-10 h-10  p-1  text-red-500";
   return (
     <footer className = "flex items-center justify-around">
-        {/* <h3 ><a href="tel:+13034752882">Phone : 303-475-2882</a></h3>  
-        <h3 ><a href="mailto: raymond.ed.lewis@gmail.com">Email : raymond.ed.lewis@gmail.com</a></h3> */}
-        <AiOutlineMail className={icon} />
-        <BsTelephone  className={icon}  />
-        <a href="https://www.linkedin.com/in/raymond-lewis-51719325/" target="_blank"><SiLinkedin  className={icon} /></a>
-        <a href="https://github.com/l1keafox" target="_blank"><SiGithub   className={icon} /></a>
+        {/* <h3 ></h3>  
+        <h3 ></h3> */}
+        <a href="tel:+13034752882"><BsTelephone className={icon} />303-475-2882</a>
+        
+        <a href="mailto: raymond.ed.lewis@gmail.com"><AiOutlineMail  className={icon}  /> raymond.ed.lewis@gmail.com</a>
+        
+        <a href="https://www.linkedin.com/in/raymond-lewis-51719325/" target="_blank"><SiLinkedin  className={icon} />www.linkedin.com/in/raymond-lewis-51719325/</a>
+        <a href="https://github.com/l1keafox" target="_blank"><SiGithub   className={icon} />github.com/l1keafox</a>
     </footer>
   )
 }
