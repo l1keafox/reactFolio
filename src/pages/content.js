d98ea1922b66a43f83cec4e0eeffb51a68a53784
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-23 16:51:23 -0600

    add anime

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,25 +1,26 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import middle from "../components/assets/images/middle.PNG";
 import projectOne from "../components/assets/images/projectOne.PNG";
 import projectThree from "../components/assets/images/bubble.png";
 import team from "../components/assets/images/team.png";
 import weather from "../components/assets/images/weather.png";
-import { Transition } from '@headlessui/react';
+import { Transition } from "@headlessui/react";
 export default function Content(prop) {
-  const [isShowing, setIsShowing] = useState(false)
+  const [isShowing, setIsShowing] = useState(false);
   useEffect(() => {
     // Update the document title using the browser API
     setIsShowing(true);
-  },[]);
+  }, []);
   useEffect(() => {
     // Update the document title using the browser API
     setIsShowing(prop.isShowing);
-  },[prop.isShowing]);
+  }, [prop.isShowing]);
 
   const portFolio = [
     {
       title: "Bubble Dungeonz",
-      description: "A platform for game developers to host multi/single player games to interact with other users. Features a multiplayer bubble popping game, saved chat groups and customized display settings",
+      description:
+        "A platform for game developers to host multi/single player games to interact with other users. Features a multiplayer bubble popping game, saved chat groups and customized display settings",
       img: projectThree,
       repoLink: "https://github.com/l1keafox/bubbleDungeonz",
       deployLink: "https://bubbledungeonz.herokuapp.com/",
@@ -27,28 +28,32 @@ export default function Content(prop) {
 
     {
       title: "Middle-Where",
-      description: "A web app to find a centralized location for a group of friends. Users can create accounts and login, using google maps to display location of friends and the central meeting spot",
+      description:
+        "A web app to find a centralized location for a group of friends. Users can create accounts and login, using google maps to display location of friends and the central meeting spot",
       img: middle,
       repoLink: "https://github.com/l1keafox/middleWhere",
       deployLink: "https://limitless-temple-38712.herokuapp.com/",
     },
     {
       title: "RealTimeCommodities",
-      description: "A web app that will pull commodities news/prices through an API call. Uses google graphs to show history of commodity prices.",
+      description:
+        "A web app that will pull commodities news/prices through an API call. Uses google graphs to show history of commodity prices.",
       img: projectOne,
       repoLink: "https://github.com/l1keafox/RealTimeCommodities",
       deployLink: "https://l1keafox.github.io/RealTimeCommodities/",
     },
     {
       title: "Weather DashBoard",
-      description: "Web app that uses api calls to get a 5 day forecast for a city.",
+      description:
+        "Web app that uses api calls to get a 5 day forecast for a city.",
       img: weather,
       repoLink: "https://github.com/l1keafox/weatherDashboard",
       deployLink: "https://l1keafox.github.io/weatherDashboard/",
     },
     {
       title: "Team maker",
-      description: "Command line interface, for an team and it will output an html.",
+      description:
+        "Command line interface, for an team and it will output an html.",
       img: team,
       repoLink: "https://github.com/l1keafox/myTeamSetting",
       deployLink: "https://l1keafox.github.io/myTeamSetting/",
@@ -58,93 +63,92 @@ export default function Content(prop) {
   //
   // style={{ width: "100%", height: "50px" }}
   // class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-  var i = 0;
   return (
     <div>
-
-
-
-          
-      <h1 className="text-center text-5xl  "> My Projects </h1>
-      <div className ='flex flex-col  py-8 gap-x-2 gap-y-1 text-center items-center justify-center'> 
-      {portFolio.map((thought, index) => (
-
-    //   <Transition
-    //   show={isShowing}
-    //   enter="transition duration-1000 ease-in-out"
-    //   enterFrom="transform translate-x-full opacity-0 "
-    //   enterTo="transform translate-x-0  opacity-100"
-    //   leave="transition duration-1000 ease-in-out"
-    //   leaveFrom="transform translate-x-0  opacity-100"
-    //   leaveTo="transform translate-y-full  opacity-0"
-    // >
-    index%2 
-?
-        <div className="max-w-sm w-3/4 lg:max-w-full lg:flex p-2 " key={index}>
-          <img
-            className="even:rotate-1 h-48 lg:h-auto w-1/2 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-
-            src={thought.img}
-          />
-          <div className="bg-slate-50 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-            <div className="mb-8">
-              <div className="text-gray-900 font-bold text-3xl mb-2">
-                {thought.title}
+      <Transition
+        show={isShowing}
+        enter="transition duration-1000 ease-in-out"
+        enterFrom="transform translate-x-full opacity-0 "
+        enterTo="transform translate-x-0  opacity-100"
+        leave="transition duration-1000 ease-in-out"
+        leaveFrom="transform translate-x-0  opacity-100"
+        leaveTo="transform translate-y-full  opacity-0"
+      >
+        <h1 className="text-center text-5xl  "> My Projects </h1>
+        <div className="flex flex-col  py-8 gap-x-2 gap-y-1 text-center items-center justify-center">
+          {portFolio.map((thought, index) =>
+            index % 2 ? (
+              <div
+                className="max-w-sm w-3/4 lg:max-w-full lg:flex p-2 "
+                key={index}
+              >
+                <img
+                  className="even:rotate-1 h-48 lg:h-auto w-1/2 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                  src={thought.img}
+                />
+                <div className="bg-slate-50 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                  <div className="mb-8">
+                    <div className="text-gray-900 font-bold text-3xl mb-2">
+                      {thought.title}
+                    </div>
+                    <p className="text-gray-700 text-base">
+                      {thought.description}
+                    </p>
+                  </div>
+                  <div className="flex items-center  justify-center">
+                    <div className="text-sm">
+                      <a href={thought.repoLink} target="_blank">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-gray-900 font-bold py-2 px-4 m-2 rounded">
+                          Repo
+                        </button>
+                      </a>
+                      <a href={thought.deployLink} target="_blank">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-gray-900 font-bold py-2 px-4 rounded">
+                          DeployLink
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <p className="text-gray-700 text-base">{thought.description}</p>
-            </div>
-            <div className="flex items-center  justify-center">
-              <div className="text-sm">
-                <a href={thought.repoLink} target="_blank">
-                  <button className="bg-blue-500 hover:bg-blue-700 text-gray-900 font-bold py-2 px-4 m-2 rounded">
-                    Repo
-                  </button>
-                </a>
-                <a href={thought.deployLink} target="_blank">
-                  <button className="bg-blue-500 hover:bg-blue-700 text-gray-900 font-bold py-2 px-4 rounded">
-                    DeployLink
-                  </button>
-                </a>
+            ) : (
+              <div
+                className="max-w-sm w-3/4 lg:max-w-full lg:flex p-2 "
+                key={index}
+              >
+                <div className="bg-slate-50 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                  <div className="mb-8">
+                    <div className="text-gray-900 font-bold text-3xl mb-2">
+                      {thought.title}
+                    </div>
+                    <p className="text-gray-700 text-base">
+                      {thought.description}
+                    </p>
+                  </div>
+                  <div className="flex items-center  justify-center">
+                    <div className="text-sm">
+                      <a href={thought.repoLink} target="_blank">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-gray-900 font-bold py-2 px-4 m-2 rounded">
+                          Repo
+                        </button>
+                      </a>
+                      <a href={thought.deployLink} target="_blank">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-gray-900 font-bold py-2 px-4 rounded">
+                          DeployLink
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <img
+                  className="odd:rotate-1 h-48 lg:h-auto w-1/2 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                  src={thought.img}
+                />
               </div>
-            </div>
-          </div>
+            )
+          )}
         </div>
-:
-<div className="max-w-sm w-3/4 lg:max-w-full lg:flex p-2 " key={index}>
-<div className="bg-slate-50 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-  <div className="mb-8">
-    <div className="text-gray-900 font-bold text-3xl mb-2">
-      {thought.title}
-    </div>
-    <p className="text-gray-700 text-base">{thought.description}</p>
-  </div>
-  <div className="flex items-center  justify-center">
-    <div className="text-sm">
-      <a href={thought.repoLink} target="_blank">
-        <button className="bg-blue-500 hover:bg-blue-700 text-gray-900 font-bold py-2 px-4 m-2 rounded">
-          Repo
-        </button>
-      </a>
-      <a href={thought.deployLink} target="_blank">
-        <button className="bg-blue-500 hover:bg-blue-700 text-gray-900 font-bold py-2 px-4 rounded">
-          DeployLink
-        </button>
-      </a>
-    </div>
-  </div>
-</div>
-<img
-  className="odd:rotate-1 h-48 lg:h-auto w-1/2 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-
-  src={thought.img}
-/>
-</div>
-    
-
-//     </Transition>
-      ))}
-      </div>
-
+      </Transition>
     </div>
   );
 }
