2f5d4121b755e948ea37baaf8288847ea2135e54
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-31 14:08:44 -0600

    Added hover tooltip and colors

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,19 +10,26 @@ import {
 import {
   AiOutlineMail
 } from "react-icons/ai";
-
+import Tooltip from '@mui/material/Tooltip';
 
 
 export default function footer() {
   const icon = "w-10 h-10  p-1  ";
   return (
     <footer className = "flex items-center justify-around font-mono py-2">
-        <a href="tel:+13034752882" className= "flex flex-col items-center justify-center text-red-500"><BsTelephone className={icon} /></a>
+      <Tooltip title="3034752882" arrow>
+        <a href="tel:+13034752882" className= "flex flex-col items-center justify-center text-red-500 hover:bg-red-200"><BsTelephone className={icon} /></a>
+      </Tooltip>        
+      <Tooltip title="raymond.ed.lewis@gmail.com" arrow>
+        <a href="mailto: raymond.ed.lewis@gmail.com" className= "flex flex-col items-center justify-center text-blue-400  hover:bg-blue-100"><AiOutlineMail  className={icon}  /></a>
+        </Tooltip>        
         
-        <a href="mailto: raymond.ed.lewis@gmail.com" className= "flex flex-col items-center justify-center text-blue-400"><AiOutlineMail  className={icon}  /></a>
-        
-        <a href="https://www.linkedin.com/in/raymond-lewis-51719325/" target="_blank" className= "flex flex-col items-center justify-center text-green-600"><SiLinkedin  className={icon} /></a>
-        <a href="https://github.com/l1keafox" target="_blank" className= "flex flex-col items-center justify-center text-yellow-300"><SiGithub   className={icon} /></a>
+        <Tooltip title="https://www.linkedin.com/in/raymond-lewis-51719325/" arrow>
+        <a href="https://www.linkedin.com/in/raymond-lewis-51719325/" target="_blank" className= "flex flex-col items-center justify-center text-green-600  hover:bg-green-300"><SiLinkedin  className={icon} /></a>
+        </Tooltip>        
+        <Tooltip title="https://github.com/l1keafox" arrow>
+        <a href="https://github.com/l1keafox" target="_blank" className= "flex flex-col items-center justify-center text-yellow-300  hover:bg-yellow-100"><SiGithub   className={icon} /></a>
+        </Tooltip>        
     </footer>
   )
 }
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -13,6 +13,8 @@ import {
   SiTypescript,
 } from "react-icons/si";
 import { FaNode, FaNpm, FaReact } from "react-icons/fa";
+import Tooltip from '@mui/material/Tooltip';
+
 export default function Landing(prop) {
   const [isShowing, setIsShowing] = useState(false)
   useEffect(() => {
@@ -22,7 +24,7 @@ export default function Landing(prop) {
     setIsShowing(prop.isShowing);
   },[prop.isShowing]);
   
-  const icon = "w-14 h-14  p-1  text-gray-500";
+  const icon = "w-14 h-14  p-1  text-gray-500 hover:bg-blue-500";
 
   return (
     <header className='flex flex-col  py-8 gap-x-2 gap-y-1 text-center items-center justify-center font-mono'>
@@ -58,20 +60,46 @@ export default function Landing(prop) {
         <br></br>
 
         <div className= "flex  justify-center items-center">
-            <SiHtml5 className={icon}></SiHtml5>
+          <Tooltip title="HTML5" arrow><div>
+            <SiHtml5  className={icon}>Hi</SiHtml5>
+          </div></Tooltip>
+          <Tooltip title="Bootstrap" arrow><div>
             <SiBootstrap className={icon}></SiBootstrap>
-            <SiJavascript className={icon}></SiJavascript>
+            </div></Tooltip>
+
+            <Tooltip title="Javascript" arrow><div>
+            <SiJavascript  className={icon}></SiJavascript>
+            </div></Tooltip>
+            <Tooltip title="NodeJs" arrow><div>
             <FaNode className={icon}></FaNode>
+            </div></Tooltip>
+            <Tooltip title="NPM" arrow><div>
             <FaNpm className={icon}></FaNpm>
+            </div></Tooltip>
+            <Tooltip title="MySQL" arrow><div>
             <SiMysql className={icon}></SiMysql>
+            </div></Tooltip>
+            <Tooltip title="MongoDB" arrow><div>
             <SiMongodb className={icon}></SiMongodb>
+            </div></Tooltip>
+            <Tooltip title="Handlebars" arrow><div>
             <SiHandlebarsdotjs className={icon}></SiHandlebarsdotjs>
+            </div></Tooltip>
+            <Tooltip title="React" arrow><div>
             <FaReact className={icon}></FaReact>            
+            </div></Tooltip>
+            <Tooltip title="Socket.io" arrow><div>
             <SiSocketdotio className = {icon} />
+            </div></Tooltip>
+            <Tooltip title="GraphQL" arrow><div>
             <SiGraphql className = {icon} /> 
+            </div></Tooltip>
+            <Tooltip title="TailWind" arrow><div>
             <SiTailwindcss className = {icon} /> 
+            </div></Tooltip>
+            <Tooltip title="TypeScript" arrow><div>
             <SiTypescript className = {icon} /> 
-            
+            </div></Tooltip>
 
         </div>
         </Transition>
